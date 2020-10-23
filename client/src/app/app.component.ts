ce5fe938d88bd8a19d987f43053ff8f5e12565de
Martin Vaudry <martinvaudry@hotmail.com>
2020-10-23 16:33:15 -0400

    Navigation et début de data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Simplex - Client';
+  products: IProduct[];
+
+  constructor(private Http: HttpClient) {}
+
+  ngOnInit(): void {
+   this.Http.get('https://localhost:5001/api/products?pagesSize=50').subscribe((response: IPagination) => {
+     this.products = response.data;
+   }, error => {
+     console.log(error);
+   });
+  }
 }
