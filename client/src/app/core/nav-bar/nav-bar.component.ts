0203bc9c95844e5a3affc444b366253813271a00
Martin Vaudry <martinvaudry@hotmail.com>
2020-12-07 15:17:32 -0500

    finalité des créations de rendu d'erreurs

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
+  templateUrl:'./nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
