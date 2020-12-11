386d927a00d30bb8f0f01d1ea4cefc6ba611a42c
Martin Vaudry <martinvaudry@hotmail.com>
2020-12-11 13:47:42 -0500

    ajout de page d'accueil et formatage du design par theme

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,14 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Accueil'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Page d\'Erreur'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Erreur de serveur'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'NON TROUVÉ'}},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Nos produits'}},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
